2f37e48471b9dea1923436d927186909d25d8072
amirmohammad.karamzadeh <amirmohammad.part@outlook.com>
2025-05-04 09:55:29 +0330

    translation: main page persian title + logo changed

--- CTFd/views.py
+++ CTFd/views.py
@@ -209,7 +209,7 @@ def setup():
     <div class="col-md-6 offset-md-3">
         <img class="w-100 mx-auto d-block" style="max-width: 500px;padding: 50px;padding-top: 14vh;" src="{default_ctf_banner_location}" />
         <h3 class="text-center">
- همینه که هست 
+            سکوی برگزاری مسابقات CTF
         </h3>
         <br>
         <h4 class="text-center">
